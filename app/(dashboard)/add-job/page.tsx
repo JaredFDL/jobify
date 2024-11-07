29e09018fd8191483535fe01414fc1de050f0af3
Jared Feng <fengdongliang002@gmail.com>
2024-11-07 23:02:16 +1000

    version revert

--- app/(dashboard)/add-job/page.tsx
+++ app/(dashboard)/add-job/page.tsx
@@ -1,5 +1,11 @@
+import CreateJobForm from "@/components/CreateJobForm";
+
 const AddJobPage = () => {
-  return <div>xxx</div>;
+  return (
+    <>
+      <CreateJobForm />
+    </>
+  );
 };
 
 export default AddJobPage;
